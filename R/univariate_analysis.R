d22553fd85ed72088cebaee817a8419b8e300835
Olivier PHILIPPE <olivier.philippe@gmail.com>
2017-03-16 22:12:12 +0000

    Various fonts fixing

--- R/univariate_analysis.R
+++ R/univariate_analysis.R
@@ -260,7 +260,7 @@
     ggplot(salaryPercentAll, aes(x=as.numeric(Salary), y=value, colour=type))+
         geom_point(size=4)+
         geom_line(size=2)+
-        geom_text(aes(label=paste(value, '%')), size=FONT_SIZE,vjust=-0.2, show.legend = FALSE)+
+        geom_text(aes(label=paste(value, '%')), size=FONT_SIZE/2,vjust=-0.2, show.legend = FALSE)+
         # geom_text(show.legend = FALSE) +  # To remove the symbol 'a' in legend because use geom_txt
         ggtitle('Comparison salary RSE vs UK')+
         theme_minimal() +
@@ -271,9 +271,9 @@
         scale_x_continuous(breaks = c(1, 2, 3, 4, 5), labels =levels(salaryPercentAll$Salary))+
         theme(plot.title = element_text(size=FONT_SIZE))+
         theme(axis.title.x = element_blank()) +
-        theme(legend.text=element_text(size=FONT_SIZE)) +
-        theme(axis.title.y =element_text(size=FONT_SIZE)) +
-        theme(axis.text =element_text(size=FONT_SIZE/1.5))
+        theme(legend.text=element_text(size=FONT_SIZE*2)) +
+        theme(axis.title.y =element_text(size=FONT_SIZE*2)) +
+        theme(axis.text =element_text(size=FONT_SIZE))
 
 ## @knitr salaryAllPlot2
     #### Same data but with a mix of line and barplots
